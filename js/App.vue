2e968bb323f51bb047f8e790e8376ec3520b6812
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2022-10-20 19:44:31 +0300

    created calculator

---
+++ js/App.vue
@@ -0,0 +1,72 @@
+<template>
+   <section id="learnVueJs">
+      <div class="counterInfo">
+         <p>Лайков набралось: {{ likes }}</p>
+         <p>Дизлайов набралось: {{ dislikes }}</p>
+      </div>
+      <div class="buttons">
+         <button @click="plusLikes">Лайк</button>
+         <button @click="plusDislikes">Дизлайк</button>
+
+         <button @click="minesLikes">Убрать лайк</button>
+         <button @click="minesDislikes">Убрать дизлайк</button>
+      </div>
+      <!-- <button>Показать попап</button>
+<div class="popup">
+    <p></p>
+</div> -->
+   </section>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         likes: 0,
+         dislikes: 0,
+      };
+   },
+   methods: {
+      plusLikes() {
+         this.likes++;
+      },
+      plusDislikes() {
+         this.dislikes++;
+      },
+      minesLikes() {
+         this.likes--;
+         if (this.likes <= 0) {
+            this.likes = 0;
+         }
+      },
+      minesDislikes() {
+         this.dislikes--;
+         if (this.dislikes <= 0) {
+            this.dislikes = 0;
+         }
+      },
+   },
+};
+</script>
+
+<style lang="scss">
+#learnVueJs {
+   position: absolute;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+   font-size: 30px;
+   .counterInfo,
+   .buttons {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+   }
+   .buttons {
+      margin-top: 20px;
+      button {
+         padding: 10px 0;
+      }
+   }
+}
+</style>
--- js/script.js
+++ js/script.js
@@ -1,9 +1,82 @@
 new Vue({
-   el: "#v-model-basic",
-   data: {
-      counter: 0,
-      price: "",
-      firstNumber: 2,
-      secondNumber: 2,
+   el: "#learnVueJs",
+   data() {
+      return {
+         postTitle: "",
+         postDescription: "",
+         postArray: [],
+
+         //  posts: [],
+
+         //  likes: 0,
+         //  dislikes: 0,
+         //  valueTitle: "",
+         //  valueBody: "",
+      };
+   },
+   methods: {
+      addPost() {
+         const addedNewPost = {
+            title: this.postTitle,
+            description: this.postDescription,
+         };
+         this.postArray.push(addedNewPost);
+         this.postTitle = "";
+         this.postDescription = "";
+      },
+   },
+   //    methods: {
+   //       createPost() {
+   //          const newPost = {
+   //             id: Date.now(),
+   //             title: this.valueTitle,
+   //             body: this.valueBody,
+   //          };
+   //          this.posts.push(newPost);
+   //          this.valueTitle = "";
+   //          this.valueBody = "";
+   //       },
+
+   //       //   plusLikes() {
+   //       //      this.likes++;
+   //       //   },
+   //       //   plusDislikes() {
+   //       //      this.dislikes++;
+   //       //   },
+   //       //   minesLikes() {
+   //       //      this.likes--;
+   //       //      if (this.likes <= 0) {
+   //       //         this.likes = 0;
+   //       //      }
+   //       //   },
+   //       //   minesDislikes() {
+   //       //      this.dislikes--;
+   //       //      if (this.dislikes <= 0) {
+   //       //         this.dislikes = 0;
+   //       //      }
+   //       //   },
+   //    },
+});
+
+new Vue({
+   el: ".calculator",
+   data() {
+      return {
+         result: "",
+         numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+         operations: ["+", "-", "*", "/"],
+      };
+   },
+   methods: {
+      input(char) {
+         this.result = this.result.toString();
+         this.result += char;
+      },
+      recet() {
+         this.result = "";
+      },
+      calc() {
+         this.result = eval(this.result);
+      },
    },
 });
